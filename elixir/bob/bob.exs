135f212ec4ab1482e92832f3af7474bf55291410
alex <alex@ameryn.com>
2019-01-11 23:39:45 -0500

    Finish bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,26 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      true -> "Whatever."
+      # Not saying anything
+      String.trim(input) == "" ->
+        "Fine. Be that way!"
+
+      # Yelling a question
+      Regex.match?(~r/[A-Я]+/, input) and input == String.upcase(input) and
+          String.ends_with?(input, "?") ->
+        "Calm down, I know what I'm doing!"
+
+      # Yelling
+      Regex.match?(~r/[A-Я]+/, input) and input == String.upcase(input) ->
+        "Whoa, chill out!"
+
+      # Asking a question
+      String.ends_with?(input, "?") ->
+        "Sure."
+
+      # Everything else
+      true ->
+        "Whatever."
     end
   end
 end
